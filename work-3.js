7bdf02611dcee7d335b7c6ef3712e9e433695b4e
이연승 <leeys951006@gmail.com>
2024-06-04 11:47:41 +0900

    this 헷갈릴때 보기

--- work-3.js
+++ work-3.js
@@ -6,3 +6,16 @@ function workThree(a, b) {
 }
 
 console.log(workThree(1, 2));
+
+let itsMe = {
+  name: '이연승',
+  age: 30,
+  home: '대전',
+  test: this,
+  sentence: function () {
+    return this.name + '은 미남이다.';
+  },
+};
+
+console.dir('객체에서 보는 this :', itsMe);
+console.log('메서드로 보는 this :', itsMe.sentence());
