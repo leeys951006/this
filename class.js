085940497af3c7222b1b52f67e88581c07b77d54
이연승 <leeys951006@gmail.com>
2024-06-04 15:00:01 +0900

    class 연습

--- class.js
+++ class.js
@@ -1,1 +1,11 @@
-console.log('class');
+class ItsMe {
+  constructor() {
+    (this.name = '이연승'), (this.age = 30), (this.home = '대전');
+  }
+  sentence() {
+    return this.name + '은 미남이다.';
+  }
+}
+
+let itsMe = new ItsMe();
+console.log(itsMe);
